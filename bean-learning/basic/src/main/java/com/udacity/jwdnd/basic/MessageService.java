5baf55126c54cce7130e0ca4a89a7755f2893fe7
saksjain <saksjain@adobe.com>
2020-11-28 21:48:36 +0530

    chat form controller binding learning

--- bean-learning/basic/src/main/java/com/udacity/jwdnd/basic/MessageService.java
+++ bean-learning/basic/src/main/java/com/udacity/jwdnd/basic/MessageService.java
@@ -1,15 +1,20 @@
 package com.udacity.jwdnd.basic;
 
+import com.udacity.jwdnd.basic.chats.ChatMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MessageService {
 
     private String message;
+
+    private List<ChatMessage> chatMessages;
 
     public MessageService(String message){
         this.message = message;
@@ -19,9 +24,13 @@ public class MessageService {
         return this.message.toUpperCase();
     }
 
-
     public String lowerCase() {
         return this.message.toLowerCase();
+    }
+
+    public void addMessages(ChatMessage chatMessage) {
+        if(this.chatMessages == null) this.chatMessages = new ArrayList<>();
+        this.chatMessages.add(chatMessage);
     }
 
     @PostConstruct
@@ -29,5 +38,9 @@ public class MessageService {
         System.out.println("creating message service bean");
     }
 
-
+    public List<ChatMessage> getChatMessages() {
+        if(this.chatMessages == null)
+            this.chatMessages = new ArrayList<>();
+        return this.chatMessages;
+    }
 }
---
+++ bean-learning/basic/src/main/java/com/udacity/jwdnd/basic/chats/ChatController.java
@@ -0,0 +1,46 @@
+package com.udacity.jwdnd.basic.chats;
+
+import com.udacity.jwdnd.basic.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    MessageService chatMessageService;
+
+    @GetMapping("/chat")
+    public String getChats(@ModelAttribute("userProfile") ChatFormPojo userProfile, Model model) {
+        System.out.println(this.chatMessageService.getChatMessages());
+        model.addAttribute("chatMessages", this.chatMessageService.getChatMessages());
+        return "chat";
+    }
+
+
+    @PostMapping("/chat")
+    public String postChats(@ModelAttribute("userProfile") ChatFormPojo userProfile, Model model) {
+        System.out.println("sakju "+userProfile);
+        ChatMessage cm = new ChatMessage();
+        switch (userProfile.getMessageType()){
+            case "Say":
+                cm.setMessageText(userProfile.getMessageText());
+                break;
+            case "whisper":
+                cm.setMessageText(userProfile.getMessageText().toLowerCase());
+                break;
+            case "SHOUT":
+                cm.setMessageText(userProfile.getMessageText().toUpperCase());
+                break;
+        }
+        cm.setUsername(userProfile.getUsername());
+        this.chatMessageService.addMessages(cm);
+        model.addAttribute("chatMessages", this.chatMessageService.getChatMessages());
+        return "chat";
+    }
+}
