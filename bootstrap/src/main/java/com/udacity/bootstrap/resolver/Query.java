26a8af71c7949e9a53d39f91fc058f57e9dfd67e
saksjain <saksjain@adobe.com>
2020-12-26 21:47:30 +0530

    query and mutation resolver

---
+++ bootstrap/src/main/java/com/udacity/bootstrap/resolver/Query.java
@@ -0,0 +1,30 @@
+package com.udacity.bootstrap.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.udacity.bootstrap.entities.Dog;
+import com.udacity.bootstrap.repository.DogRepository;
+import com.udacity.bootstrap.services.DogNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+
+    @Autowired
+    DogRepository dogRepository;
+
+    public Iterable<Dog> findAllDogs(){
+        return dogRepository.findAll();
+    }
+
+    public Dog findDogBreedById(Long id) {
+        Optional<Dog> dog = dogRepository.findById(id);
+        if(dog.isPresent())
+            return dog.get();
+        else
+            throw new DogNotFoundException("DogBreed with id "+id+" Not Found");
+    }
+
+}
