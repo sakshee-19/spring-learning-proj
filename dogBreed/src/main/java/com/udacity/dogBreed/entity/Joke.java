052ce717b1201b3f2e82c516f8d322c58c47c5ad
saksjain <saksjain@adobe.com>
2020-12-28 14:15:45 +0530

    consume rest api via rest template

---
+++ dogBreed/src/main/java/com/udacity/dogBreed/entity/Joke.java
@@ -0,0 +1,50 @@
+package com.udacity.dogBreed.entity;
+
+public class Joke {
+    private Long id;
+    private String type;
+    private String setup;
+    private String punchline;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getPunchline() {
+        return punchline;
+    }
+
+    public void setPunchline(String punchline) {
+        this.punchline = punchline;
+    }
+
+    @Override
+    public String toString() {
+        return "Joke{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", setup='" + setup + '\'' +
+                ", punchline='" + punchline + '\'' +
+                '}';
+    }
+}
