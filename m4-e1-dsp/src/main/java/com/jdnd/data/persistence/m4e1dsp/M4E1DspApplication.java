f957c813abfa72bf98f9e9253b0be98869ef6fa6
saksjain <saksjain@adobe.com>
2021-01-04 22:56:41 +0530

    data persistence init project

---
+++ m4-e1-dsp/src/main/java/com/jdnd/data/persistence/m4e1dsp/M4E1DspApplication.java
@@ -0,0 +1,13 @@
+package com.jdnd.data.persistence.m4e1dsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M4E1DspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M4E1DspApplication.class, args);
+	}
+
+}
