f1b9fa04414eb6fa86de31fa421af0f54bf078f7
saksjain <saksjain@adobe.com>
2021-01-07 22:43:05 +0530

    bean config datasource

---
+++ m4-e1-dsp/src/main/java/com/jdnd/data/persistence/m4e1dsp/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.jdnd.data.persistence.m4e1dsp.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource getDataSource() {
+        DataSourceBuilder dsb = DataSourceBuilder.create();
+        dsb.url("jdbc:mysql://localhost:3306/plant");
+        return dsb.build();
+    }
+}
