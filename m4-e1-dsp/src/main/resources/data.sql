00f5268e13f1328214f27ea83ba53657b422ceec
saksjain <saksjain@adobe.com>
2021-01-09 17:01:49 +0530

    insert ignore to skip data insert when present

--- m4-e1-dsp/src/main/resources/data.sql
+++ m4-e1-dsp/src/main/resources/data.sql
@@ -3,4 +3,4 @@
 --       name = 'Gummy Badgers',
 --       price = '3.50';
 
-insert into candy (id,name,price) values (1,"Gummy Badgers","3.50");
+insert ignore into candy (id,name,price) values (1,"Gummy Badgers","3.50");
